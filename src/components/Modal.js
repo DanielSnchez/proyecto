d490a8db12c4074ced865e3fa7466de5dd5ad072
DanielSnchez <43664070+DanielSnchez@users.noreply.github.com>
2022-06-26 18:52:42 -0500

    Update Modal.js
    Commit Login

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,13 +7,15 @@ const Modal = ({
     cambiarEstado,
     titulo = "Alerta",
     mostrarHeader,
-    mostrarOverlay
+    mostrarOverlay,
+    mostrarBotonCerrar,
+    overlayLogin
 }) => {
 
     return (
         <>
             {estado &&
-                <Overlay mostrarOverlay={mostrarOverlay}>
+                <Overlay mostrarOverlay={mostrarOverlay} overlayLogin={overlayLogin}>
                     <ContenedorModal>
                         {mostrarHeader &&
                             <EncabezadoModal>
@@ -21,11 +23,13 @@ const Modal = ({
                             </EncabezadoModal>
                         }
                         {children}
-                        <BotonCerrar onClick={() => cambiarEstado(false)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                            </svg>
-                        </BotonCerrar>
+                        {mostrarBotonCerrar &&
+                            <BotonCerrar onClick={() => cambiarEstado(false)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                </svg>
+                            </BotonCerrar>
+                        }
                     </ContenedorModal>
                 </Overlay>
             }
@@ -52,6 +56,7 @@ const Overlay = styled.div`
     top: 0;
     left: 0;
     background: ${props => props.mostrarOverlay ? 'rgba(0, 0, 0, 0.1)' : 'rgba(0, 0, 0, 0)'};
+    background: ${props => props.overlayLogin ? 'rgba(0, 0, 0, 0.9)' : 'rgba(0, 0, 0, 0.1)'};
     padding: 40px;
     box-sizing: border-box;
     display: flex;
